87ad3fdac375f1878b3ba503096e78de97f764ac
Rodris07 <htrrodrigo9@gmail.com>
2024-10-23 21:10:31 -0300

    Formulario de Contacto
    Campos Validados

--- Java.js
+++ Java.js
@@ -1,4 +1,4 @@
-let imgArray = ['Imagenes/Playstation carrusel.jpg', 'Imagenes/xbox carrusel.webp','Imagenes/Nintendo carrusel.jpg'] ;
+let imgArray = ['Imagenes/Playstation carrusel.jpg', 'Imagenes/xbox carrusel.webp', 'Imagenes/Nintendo carrusel.jpg'];
 let indiceActual = 0;
 
 let boton = document.getElementById('Siguiente');
@@ -7,24 +7,25 @@ let img = document.getElementById("imgCarrusel");
 
 
 function Derecha() {
-indiceActual++;
-if (indiceActual >= imgArray.length) {
-    indiceActual = 0;
-    
-}
+    indiceActual++;
+    if (indiceActual >= imgArray.length) {
+        indiceActual = 0;
 
-img.src = imgArray[indiceActual];   
+    }
+
+    img.src = imgArray[indiceActual];
 }
 function Izquierda() {
     indiceActual--;
-    if (indiceActual <0) {
-        indiceActual = imgArray.length -1;
-        
+    if (indiceActual < 0) {
+        indiceActual = imgArray.length - 1;
+
     }
-    
-    img.src = imgArray[indiceActual];      
-    }   
-  
-boton.addEventListener("click" ,Derecha);
+
+    img.src = imgArray[indiceActual];
+}
+
+boton.addEventListener("click", Derecha);
 botonI.addEventListener("click", Izquierda);
-setInterval(Derecha , 9000); 
+setInterval(Derecha, 9000);
+
